843a067de26f1e3f37a57bf277d1bf56e32c1df4
Anziya-nazar <anziyanazar26@gmail.com>
2024-01-04 20:06:39 +0530

    Jscommit1

---
+++ script.js
@@ -0,0 +1,60 @@
+let person={
+name:"Anziya",
+age:20,
+place:"kollam",
+}
+// console.log(person.age);
+// let x=8;
+// let y=9;
+// if(x>y){
+// console.log('x is greater than y');}
+
+//       {   elseif(x==y)
+// console.log('both are equal');}
+// else(x<y){
+//     console.log('x is lower than y');
+// }
+// //functions
+function addval(ad1,ad2) {
+    var sum=ad1+ad2;
+    return sum;
+}
+var result=addval(10,20);
+console.log('the result is'+result)
+
+function addval(ad1,ad2) {
+    var sum=ad1-ad2;
+    return sum;
+}
+var result=addval(40,20);
+console.log('the result is'+result)
+     
+function addval(ad1,ad2) {
+    var sum=ad1*ad2;
+    return sum;
+}
+var result=addval(11,12);
+console.log('the result is'+result)
+    
+
+     
+function addval(ad1,ad2) {
+    var sum=ad1%ad2;
+    return sum;
+}
+var result=addval(40,4);
+console.log('the result is'+result)
+    
+// for(let i=0,i<10,i++) {
+//     console.log(i+1);
+// }
+var animal=['tiger','lion','cat','dog','cow'];
+var x= animal.length
+for(var i=0; i<x;i++){
+    console.log(animal[i])
+
+}
+var animal=['tiger','lion','cat','dog','cow'];
+var x= animal.length
+while(leti=0; j<x;i++) {
+    console.log(animal[i])}
